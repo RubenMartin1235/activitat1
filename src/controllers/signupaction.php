bc168c40e1be07d221ff54a530ad4c04cb260768
dev <r.martin1dawnuria2021@gmail.com>
2022-10-11 17:38:06 +0200

    commit 6

--- src/controllers/signupaction.php
+++ src/controllers/signupaction.php
@@ -21,11 +21,18 @@
 			isset($_POST['passwd']) && isset($_POST['passwdConfirm']) &&
 			isset($_POST['isProf'])
 		) {
+			/*
 			$fullname = $_REQUEST['fullname'];
 			$email = $_REQUEST['email'];
 			$passwd = $_REQUEST['passwd'];
 			$passwdConfirm = $_REQUEST['passwdConfirm'];
 			$isProf = $_REQUEST['isProf'];
+			*/
+			$fullname = filter_input(INPUT_POST, 'fullname', FILTER_SANITIZE_STRING);
+			$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+			$passwd = filter_input(INPUT_POST, 'passwd', FILTER_SANITIZE_STRING);
+			$passwdConfirm = filter_input(INPUT_POST, 'passwdConfirm', FILTER_SANITIZE_STRING);
+			$isProf = filter_input(INPUT_POST, 'isProf', FILTER_SANITIZE_STRING);
 			
 			if (
 				(
